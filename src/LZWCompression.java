4e8940fc5b88ca394f7653990ef9f6861490ef22
solomonbaik <52802497+solomonbaik@users.noreply.github.com>
2021-09-04 17:11:40 -0700

    working adaptable dictionary size + comments

--- src/LZWCompression.java
+++ src/LZWCompression.java
@@ -2,16 +2,15 @@ import java.util.*;
 import java.io.*;
 public class LZWCompression {
 	
-	static BufferedReader in;
 	static PrintWriter out;
-	static String input = "";
-	static Scanner s;
+	static String inputString = "";
+	static Scanner in;
+	static int bitSize = 8;
 	static LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();
 	
 	public static void main(String[] args) throws IOException{
-		in = new BufferedReader(new FileReader(new File("lzw-file2.txt")));
 		out = new PrintWriter(new File("output.txt"));
-		s = new Scanner(new File("input.txt"));
+		in = new Scanner(new File("input.txt"));
 		
 		init();
 		solve();
@@ -21,17 +20,45 @@ public class LZWCompression {
 	}
 	
 	static void init() throws IOException{
-		while(s.hasNext()) {
-			input += s.nextLine() + "\n";
+		while(in.hasNext()) {
+			inputString += in.nextLine() + "\n";
 		}
 		for(int i = 0; i < 256; i++) {
 			map.put(Integer.toBinaryString(i), "" + (char)(i));
 		}
-
 	}
 	
 	static void solve() {
-		out.print(input.substring(0, input.length() - 2));
+		
+		int remainingSpaces = 0; //Counts the available spaces left in the dictionary.
+		int index = 0; //Will track our current index inside the input string.
+		String currentEntry = ""; //Holds our current string every iteration.
+		String ans = ""; //Our answer string to be output at the very end.
+		while(index < inputString.length()) { //Parse through each index of the input string.
+			if(remainingSpaces == 0) { //If dictionary runs out of space, increase bit size by 1.
+				remainingSpaces = (int) Math.pow(2, bitSize++);
+				//Updating our dictionary with keys 1-bit longer:
+				LinkedHashMap<String, String> temporaryMap = new LinkedHashMap<String, String>();
+				for(String s: map.keySet()) {
+					temporaryMap.put("0" + s, map.get(s));
+				}
+				map.clear();
+				for(String s: temporaryMap.keySet()) {
+					map.put(s, temporaryMap.get(s));
+				}
+				//
+			}
+			//Need to update information every iteration:
+			remainingSpaces--; //Subtract one remaining space in the dictionary.
+			currentEntry += inputString.charAt(index++); //Update our current entry string.
+			System.out.println(currentEntry);
+//			if(map.containsValue(currentEntry)) {
+//				continue;
+//			}else {
+//
+//			}
+		}
+		out.print(inputString.substring(0, inputString.length() - 2));
 		for(String str: map.keySet()) {
 			out.println(str + ": " +  map.get(str));
 		}
